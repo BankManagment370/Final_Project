b4691e40fcc84d01a13c52211326889c1a843819
Brandon Livingston <lioncub12@gmail.com>
2018-04-09 16:17:38 -0400

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,28 @@
+#include "menu.h"
+#include "splash.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	system("mode 650");
+	system("Color 1A");
+	system("cls");
+
+	//http://www.kammerl.de/ascii/AsciiSignature.php
+	string art = "";
+
+
+	  art += "___  ____ _  _ _  _    _  _ ____ _  _ ____ ____ ____ _  _ ____ _  _ ___    ____ _   _ ____ ___ ____ _  _\n"
+		art += "|__] |__| |\ | |_/     |\/| |__| |\ | |__| | __ |___ |\/| |___ |\ |  |     [__   \_/  [__   |  |___ |\/|\n"
+		art += "|__] |  | | \| | \_    |  | |  | | \| |  | |__] |___ |  | |___ | \|  |     ___]   |   ___]  |  |___ |  |\n"
+		art += "                                                                                                        \n"
+
+	cout << art;
+	cin.get();
+	splashScreen();
+	mainMenu();
+	return 0;
+}
