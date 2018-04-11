7953c53be223adf5e0b56acc75e716dfc5d7fe06
Brandon Livingston <lioncub12@gmail.com>
2018-04-11 12:37:13 -0400

    Create Bank_Menu.h

---
+++ Bank_Menu.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#ifndef BANK_MENU_H					
+#define BANK_MENU_H
+
+
+void splashScreen();
+voidgetAccount();
+void javaProgramming();
+void androidProgramming();
+void iosProgramming();
+void mainMenu();
+int validate(string input, string checkType);
+void userInput();
+#endif
