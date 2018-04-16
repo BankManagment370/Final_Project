79c227a52a9d550656588597a6fe41438e495d2a
davidhartglass <36768373+davidhartglass@users.noreply.github.com>
2018-04-16 16:07:46 -0400

    Update Banking_functions.cpp

--- Banking_functions.cpp
+++ Banking_functions.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "Banking_Functions.h"
 #include <fstream>
+#include <vector>
+#include <sstream>
+#include "Alt.h"
 
 using namespace std;
 
@@ -64,4 +67,61 @@ void bankSkeleton::depositChecking(double d)
 {
 	cout << "Enter amount" << endl;
 	cin >> d;
+}
+
+void bankSkeleton::readDataFromFile()
+{
+	string fileName;
+	cout << "Enter a file to read from: " << endl;
+	cin >> fileName;
+
+	ifstream myFile;
+	myFile.open(fileName);
+
+
+	stringstream ss;
+	vector<string> result;
+
+	while (ss.good())
+	{
+		string fName, lName, savingBal, checkingBal;
+		int counter = 0;
+
+		while (counter < 5)
+		{
+			string substr;
+			getline(ss, substr, ',');
+			result.push_back(substr);
+
+			if (counter == 0)
+			{
+				fName = substr;
+				counter++;
+			}
+			else if(counter == 1)
+			{
+				lName = substr;
+				counter++;
+			}
+			else if(counter == 2)
+			{
+				savingBal = substr;
+				counter++;
+			}
+			else
+			{
+				checkingBal = substr;
+				counter =0;
+			}
+			gotoxy(50, 70);
+			cout << "First name: " << fName << endl;
+			gotoxy(50, 71);
+			cout << "Last name: " << fName << endl;
+			gotoxy(50, 72);
+			cout << "Savings Account Bal: " << fName << endl;
+			gotoxy(50, 73);
+			cout << "Checking account Bal: " << fName << endl;
+
+		}
+	}
 }
