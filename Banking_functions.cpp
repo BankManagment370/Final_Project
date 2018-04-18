a2822ef0e003e68a33dc407529baff0426d97a1f
davidhartglass <36768373+davidhartglass@users.noreply.github.com>
2018-04-18 14:54:03 -0400

    Update Banking_functions.cpp

--- Banking_functions.cpp
+++ Banking_functions.cpp
@@ -53,9 +53,60 @@ void bankSkeleton::setBalanceC(double balanceC) {
 	balanceC = balanceC;
 }
 
-void bankSkeleton::withdrawSavings(double w) {
-	cout<<"Enter amount"<<endl;
-	cin>>w;
+void bankSkeleton::withdrawSavings() {
+	string fName, lName, accType;
+	double w;
+	
+	gotoxy(50, 32);
+	cout << "What is the FIRST name of the account you are trying to withdraw to?" << endl;
+	gotoxy(50, 33);
+	cin >> fName;
+	
+	gotoxy(50, 34);
+	cout << "What is the LAST name of the account you are trying to withdraw to?" << endl;
+	gotoxy(50, 35);
+	cin >> lName;
+
+	gotoxy(50, 36);
+	cout << "Which account would you like to withdraw from? \n NOTE: Please use(s) for savings or (c) for checking..." << endl;
+	
+	gotoxy(50, 38);
+	cin >> accType;
+
+	gotoxy(50, 40);
+	cout << "Enter amount" << endl;
+
+	gotoxy(50, 41);
+	cin >> w;
+
+
+	if (myNode->firstName == fName && myNode->lastName == lName && accType == "s" || "S")
+	{
+		if (myNode->savingsBalance < w)
+		{
+			gotoxy(50, 41);
+			cout << "Node saving Balance for " << myNode->firstName << " "<< myNode->savingsBalance << endl;
+			gotoxy(50, 41);
+			cout << "ERROR: Cannot withdraw more than account balance, please try again" << endl;
+			gotoxy(50, 42);
+			cout << "Enter amount: " << endl;
+			gotoxy(50, 43);
+			cin >> w;
+		}
+		else
+		{
+			gotoxy(50, 44);
+			cout << "Balance before withdrawal: " << myNode->savingsBalance << endl;
+			myNode->savingsBalance -= w;
+
+			gotoxy(50, 45);
+			cout << "Balance after withdrawal: " << myNode->savingsBalance << endl;
+		}
+	}
+	else
+	{
+		myNode = myNode->next;
+	}
 }
 
 void bankSkeleton::depositSavings(double d) {
@@ -76,14 +127,11 @@ void bankSkeleton::depositChecking(double d)
 
 void bankSkeleton::readDataFromFile()
 {
-	string fileName;
+	string fileName = "data.csv";
 	string line = "";
 	ifstream myFile;
 	stringstream ss;
 	int count = 1, rows = 0, numbOfTimes = 0;
-
-	cout << "Enter a file to read from: " << endl;
-	cin >> fileName;
 
 	myFile.open(fileName);
 	
@@ -98,26 +146,30 @@ void bankSkeleton::readDataFromFile()
 
 	string fName = "";
 	string lName = "";
-	string savingBal = "";  
-	string checkingBal = ""; 
+	string savingBalAsString;
+	string checkingBalAsString;
+	double savingBal;  
+	double checkingBal; 
 
 	while (!ss.eof())
 	{
 		getline(ss, fName, ':');
 		getline(ss, lName, ':');
-		getline(ss, savingBal, ':');
-		getline(ss, checkingBal, '\n');
+		getline(ss, savingBalAsString, ':');
+		getline(ss, checkingBalAsString, '\n');
 
-	
+		savingBal = stod(savingBalAsString);
+		checkingBal = stod(checkingBalAsString);
+
 		cout << endl;
 		
 		if (count == 1)
 		{
-			myFuncs.add(fName, lName, stod(savingBal), stod(checkingBal));
+			myFuncs.add(fName, lName, savingBal, checkingBal);
 			count++;
 		}
-		else if (count >= 2) {
-			myFuncs.add(fName, lName, stod(savingBal), stod(checkingBal));
+		else if (count > 1) {
+			myFuncs.add(fName, lName, savingBal, checkingBal);
 		}
 		count++;
 	}
