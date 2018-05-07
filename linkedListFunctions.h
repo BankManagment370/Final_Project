61eda19a7a619fcbd841df493fa2f9d2c88810bd
davidhartglass <36768373+davidhartglass@users.noreply.github.com>
2018-05-07 12:25:10 -0400

    Update linkedListFunctions.h

--- linkedListFunctions.h
+++ linkedListFunctions.h
@@ -1,5 +1,6 @@
 #include "MyNode.h"
 
+
 class LinkedListFunctions {
 public:
 	LinkedListFunctions(void);	// constructor
@@ -8,24 +9,23 @@ public:
 	bool isEmpty() const;
 	bool isFull() const;
 	void makeEmpty();
-	void insertBeg(string fName, string lName, double savingsBal, double checkingBal);
-	void insertEnd(string fName, string lName, double savingsBal, double checkingBal);
-	void insertNode(int pos, string fName, string lName, double savingsBal, double checkingBal);
+
 	int findAccount(string fName, string lName);
 	void deleteNode(string fName, string lName);
-	void deleteAtPosition(int pos);
 	int getLength() const;
 	void displayList(void);
 	void deleteBeg();
 	void deleteEnd();
 	void add(string fName, string lName, double savingsBal, double checkingBal);
-	void sorting();
+
 	void savingsWithdraw(string fName, string lName);
 	void checkingWithdraw(string fName, string lName);
 	void checkingDeposit(string fName, string lName);
 	void savingsDeposit(string fName, string lName);
 	void readDataFromFile();
 	void save();
+	void showRecent();
+	void deleteRecent();
 private:
 	int length;
 	Node* head;
